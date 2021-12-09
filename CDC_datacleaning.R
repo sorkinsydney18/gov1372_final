2eaf196fe9722181b729e4f26642b2586eb73ea6
Sydney Sorkin <ssorkin@college.harvard.edu>
2021-12-09 16:54:02 -0500

    cleaned CDC covid data

--- CDC_datacleaning.R
+++ CDC_datacleaning.R
@@ -5,5 +5,44 @@ library(tidyverse)
 cdc_data <- read_csv("United_States_COVID-19_Cases_and_Deaths_by_State_over_Time.csv")
 
 
-filtered <- cdc_data %>% 
-  filter(submission_date >= as.Date("3/01/2020") & submission_date <= as.Date("7/01/2020"))
+cdc_cleaned <- cdc_data %>% 
+  mutate(submission_date = as.Date(submission_date, "%m/%d/%Y")) %>% 
+  
+  #filter for nationscape time period
+  
+  filter(submission_date >= as.Date("2020-03-01") & submission_date <= as.Date("2020-07-01")) %>% 
+  
+  #clean health jurisdictions
+  
+
+  filter(!state %in% c("AS", "GU", "MP", "PR", "VI", "FSM", "RMI", "PW")) %>%
+  
+ 
+  #national outbreak numbers
+   group_by(submission_date) %>% 
+  
+  mutate(US_tot_cases = sum(tot_cases),
+         US_new_cases = sum(new_case),
+         US_tot_death = sum(tot_death),
+         US_new_death = sum(new_death))  %>%
+  
+  select(submission_date, state, tot_cases, new_case, 
+         US_tot_cases, US_new_cases, 
+         tot_death, new_death, US_tot_death, US_new_death)
+
+save(cdc_cleaned, file = "cdc_cleaned.RData")
+  
+
+         
+
+
+
+  
+  
+  
+ 
+  
+  
+  
+
+
